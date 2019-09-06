2f6bcc521c715a62e7ae013190f2838f4c884669
sungki0152 <youbyme@gmail.com>
2019-09-06 13:16:56 -0400

    Added a scene for 4 fishes and controlling them.

--- Assets/Scripts/MoveKeyboard.cs
+++ Assets/Scripts/MoveKeyboard.cs
@@ -6,7 +6,12 @@ public class MoveKeyboard : MonoBehaviour
 {
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.A)) move += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) move += Vector3.right;
+        if (Input.GetKey(KeyCode.W)) move += Vector3.up;
+        if (Input.GetKey(KeyCode.S)) move += Vector3.down;
+
         transform.position += move * 8f * Time.deltaTime;
     }
 }
--- Assets/Scripts/MoveXboxController.cs
+++ Assets/Scripts/MoveXboxController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class MoveXboxController : MonoBehaviour
 {
+    public bool bRight = false;
+    Vector3 move;
+
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("XboxHorizontal"), Input.GetAxis("XboxVertical"), 0);
+        if(!bRight)
+            move = new Vector3(Input.GetAxis("XboxLeftHorizontal"), Input.GetAxis("XboxLeftVertical"), 0);
+        else
+            move = new Vector3(Input.GetAxis("XboxRightHorizontal"), Input.GetAxis("XboxRightVertical"), 0);
+
         transform.position += move * 8f * Time.deltaTime;
     }
 }
