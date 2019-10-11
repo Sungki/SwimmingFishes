06691afa10da983325712bbf25783965b6c0d21c
sungki0152 <youbyme@gmail.com>
2019-10-11 14:02:15 -0400

    Added  a collision avoidance class temporally for the fish movement.

---
+++ Assets/Scripts/Actions/CollisionAvoidance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAvoidance : MonoBehaviour
+{
+    const float MAX_SEE_AHEAD = 3f;
+    public GameObject obstacle;
+
+    FollowTarget ft;
+    void Start()
+    {
+        ft = GetComponent<FollowTarget>();
+    }
+
+
+    public Vector3 Avoidance()
+    {
+        Vector3 avoidance_force = Vector3.zero;
+        Vector3 ahead = transform.position + ft.velocity.normalized * MAX_SEE_AHEAD;
+        if (Vector2.Distance(ahead, obstacle.transform.position) < obstacle.GetComponent<CircleCollider2D>().radius)
+        {
+            ft.isCollision = true;
+            avoidance_force = ahead - obstacle.transform.position;
+            avoidance_force = avoidance_force.normalized;
+
+            Invoke("ReStart", 0.3f);
+        }
+
+        return avoidance_force;
+    }
+
+/*    void Update()
+    {
+        Vector3 ahead = transform.position + ft.velocity.normalized * MAX_SEE_AHEAD;
+
+//        Debug.DrawLine(transform.position, ahead, Color.red);
+
+        if (Vector2.Distance(ahead, obstacle.transform.position) < obstacle.GetComponent<CircleCollider2D>().radius)
+        {
+            ft.isCollision = true;
+            Vector3 avoidance_force = ahead - obstacle.transform.position;
+            ft.velocity += avoidance_force.normalized;
+
+            //            Debug.DrawLine(transform.position, ft.velocity, Color.blue);
+//            Invoke("ReStart", 0.3f);
+        }
+    }
+    */
+    void ReStart()
+    {
+        ft.isCollision = false;
+    }
+}
--- Assets/Scripts/Actions/FollowTarget.cs
+++ Assets/Scripts/Actions/FollowTarget.cs
@@ -8,16 +8,34 @@ public class FollowTarget : MonoBehaviour
     public float speed = 5f;
     [HideInInspector] public Vector3 velocity;
 
+    public bool isCollision = false;
+
+/*    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }*/
+
     void Update()
     {
         Vector3 targetPos = target.transform.position;
         float angleRad = Mathf.Atan2(targetPos.y - this.transform.position.y, targetPos.x - this.transform.position.x);
         float angleDeg = (180 / Mathf.PI) * angleRad;
 
-        this.transform.rotation = Quaternion.Euler(0, 0, angleDeg);
+        if(!isCollision)
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, angleDeg);
+            velocity = targetPos - transform.position;
+        }
 
-        velocity = targetPos - transform.position;
+        velocity = velocity + GetComponent<CollisionAvoidance>().Avoidance();
+
+//        velocity.x = Mathf.Clamp(velocity.x, -0.5f, 0.5f);
+//        velocity.y = Mathf.Clamp(velocity.y, -0.5f, 0.5f);
+
         velocity.z = transform.position.z;
         transform.position = transform.position + velocity * Time.deltaTime * speed;
+//        rb.MovePosition(transform.position + velocity * Time.deltaTime * speed);
     }
 }
